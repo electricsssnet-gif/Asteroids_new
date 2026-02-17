6172b6ad19eee349922bafd770d6724ee6ed530b
electricsssnet <electricsssnet@icloud.com>
2026-02-17 15:01:47 -0500

    Moving Left and Right

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
+
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-# or import everything: from module_name import *
-
-
 from logger import log_state
-
-
-
-
 
 def main():
     pygame.init()
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
+    pygame.time.Clock()
+    dt = 0
+    # inside main, before the loop
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        # print(f"Delta time: {dt}")
+
+def player():
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
 
 if __name__ == "__main__":
