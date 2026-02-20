cfcbe0353524fce9cd1313c3c39b2931c37d7201
electricsssnet <electricsssnet@icloud.com>
2026-02-19 22:57:46 -0500

    Asterid fly and collide with player, game over.

---
+++ Asteroid.py
@@ -0,0 +1,19 @@
+
+
+import pygame
+from CircleShape import CircleShape
+from constants import LINE_WIDTH
+
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
+        #self.y += CircleShape.velocity.y * dt  # Example movement
+
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
+import sys
+from tokenize import group
 import pygame
+from Asteroid import Asteroid
 from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -11,12 +16,28 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     
     pygame.time.Clock()
     dt = 0
-    # inside main, before the loop
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+ 
+    #player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
+
     
     while True:
         log_state()
@@ -25,19 +46,26 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-        player.update(dt)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collide_with(player):
+                log_event("player_hit")
+                print ("Game over!")
+                sys.exit()
+            
 
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
         # print(f"Delta time: {dt}")
 
-def player():
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
+#def player():
+#    x = SCREEN_WIDTH / 2
+ #   y = SCREEN_HEIGHT / 2
 
 
 if __name__ == "__main__":
